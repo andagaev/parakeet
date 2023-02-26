e3386d956dc07f741084084b5b6285e28551881c
Andrei Agaev <andagaev@gmail.com>
2023-02-25 18:47:46 -0800

    Break test "Boot"

--- src/test/java/com/andagaev/parakeet/ControllerTest.java
+++ src/test/java/com/andagaev/parakeet/ControllerTest.java
@@ -24,6 +24,6 @@ public class ControllerTest {
     public void getIndex() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+                .andExpect(content().string(equalTo("Greetings from Spring Booot!")));
     }
 }
